0cc20aac63951159431f15a87b60b88cff9365d6
Artem Androsenko <tyoma.and@gmail.com>
2023-06-08 15:22:22 +0300

    Add css module for Button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,35 +1,38 @@
 // import "./Button.css";
-import styled from "styled-components";
+// import styled from "styled-components";
+import style from './Button.module.css';
 
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  color: white;
-  background: #00358b;
-  border-radius: 4px;
-  box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
-  cursor: pointer;
+// const Button = styled.button`
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   color: white;
+//   background: #00358b;
+//   border-radius: 4px;
+//   box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
+//   cursor: pointer;
 
-  @media(max-width: 700px) {
-    width: 100%;
-  }
+//   @media(max-width: 700px) {
+//     width: 100%;
+//   }
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #245fbd;
-    box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
-  }
-`;
-// const Button = (props) => {
-//   return (
-//     <button type={props.type} className="button">
-//       {props.children}
-//     </button>
-//   );
-// };
+//   &:hover,
+//   &:active {
+//     background: #245fbd;
+//     box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
+//   }
+// `;
+
+const Button = (props) => {
+  return (
+    // <button type={props.type} className="button">
+    <button type={props.type} className={style.button}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
